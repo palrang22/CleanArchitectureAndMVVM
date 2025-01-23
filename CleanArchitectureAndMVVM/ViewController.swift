9c5830681353e1bfd29b4a633e5ad703838b243f
palrang22 <kseunghui@gmail.com>
2025-01-23 12:03:29 +0900

    Initial Commit

---
+++ CleanArchitectureAndMVVM/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CleanArchitectureAndMVVM
+//
+//  Created by 김승희 on 1/23/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ CleanArchitectureAndMVVMTests/CleanArchitectureAndMVVMTests.swift
@@ -0,0 +1,17 @@
+//
+//  CleanArchitectureAndMVVMTests.swift
+//  CleanArchitectureAndMVVMTests
+//
+//  Created by 김승희 on 1/23/25.
+//
+
+import Testing
+@testable import CleanArchitectureAndMVVM
+
+struct CleanArchitectureAndMVVMTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
